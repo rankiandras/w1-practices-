537f203462d62fecb6d6a9d62d09c5a4abfa3717
rankiandras <rankiandras@gmail.com>
2022-01-20 11:38:28 +0100

    index html, script.js, style.css are modified

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,46 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+];
+ */
+
+const data = [
+    {
+        name:"Gergely Kiss",
+        favoriteFood: "csirke/rizs"
+    },
+    {
+        name:"András Varga",
+        favoriteFood: "brassói"
+    },
+    {
+        name:"Buczkó Balázs",
+        favoriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name:"Krisztián Bui",
+        favoriteFood: "banán"
+    },
+    {
+        name:"Borbála Teréz Kovács",
+        favoriteFood: "avokádó"
+    },
+]
 function loadEvent() {
     console.log ("Az oldal betöltődött");
 
-    const list = document.querySelectorAll ("section");
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
-        `);
-        //elem.querySelector("h1").outerHTML = ""
+    for (const frontendStudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendStudent.name}</h1>
+                <p>${frontendStudent.favoriteFood}</p>
+            </section> 
+        `)        
     }
 }
 window.addEventListener ("load", loadEvent);
